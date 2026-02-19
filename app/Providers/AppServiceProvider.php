89b76350bf2dfb378e5d32dd754afa4800277d48
Asnaui Pangcatan <tdh.asnaui@gmail.com>
2026-02-19 12:10:40 +0800

    DONE

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
          if (config('app.env') !== 'local') {
-        // Force all URLs to use HTTP
-        URL::forceScheme('http');
-          }
+            URL::forceScheme('https');
+        }
     }
 }
